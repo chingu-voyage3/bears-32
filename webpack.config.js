2be1315da2e4e46d830cc2f2be2c13a9a6e613a0
Chaim Lando <chiptus@gmail.com>
2017-12-26 17:22:21 -0800

    basic config for babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
   }
 };
