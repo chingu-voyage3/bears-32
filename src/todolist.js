d74f8c0c4f39b9ba36d7820e28db2f94cd6e219b
dabingcheerup <yuting_xie@student.uml.edu>
2017-12-27 18:06:29 -0500

    define elements of a list item
    A list item on the to-do list has li tags, a checkBox, an input for editing task name, an edit button, and a delete button.

---
+++ src/todolist.js
@@ -0,0 +1,39 @@
+var taskInput = document.getElementsById("new-task-input");
+var addButton = document.getElementsByTagName("button")[0];
+var todolistHolder = document.getElementsById("todolist-container");
+
+//New Task List item
+var createNewTaskElement = function(taskString){
+	//Create List Item
+	var listItem = document.createElement("li");
+	//input(checkbox)
+	var checkBox = document.createElement("input");
+	//label
+	var label = document.createElement("label");
+	//input(text) task name
+	var editInput = document.createElement("input");
+	//button.edit
+	var editButton = document.createElement("button");
+	//button.delete
+	var deleteButton = document.createElement("button");
+
+	checkBox.type = "checkbox";
+	editInput.type = "text";
+
+	editButton.innerText = "Edit";
+	editButton.className = "edit";
+	deleteButton.innerText = "Delete";
+	deleteButton.className = "delete";
+
+	label.innerText = taskString;
+
+	//appending each element to listItem
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(editButton);
+	listItem.appendChild(deleteButton);
+
+	return listItem;
+}
+
