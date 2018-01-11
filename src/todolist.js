9a4a56aa2be3cb3ab1fc16108738e9b1d50bc8e9
dabingcheerup <yuting_xie@student.uml.edu>
2018-01-11 17:36:44 -0500

    use jquery-ui-bundle to make elements sortable
    run command:
    npm install --save jquery-ui-bundle
    
    index.js:
    
    'use strict';
    
    import 'jquery-ui-bundle';
    
    $(function() {
        $( "#sortable" ).sortable();
    });
    
    webpack.config.js:
    
      plugins: [
          new webpack.ProvidePlugin({
              $: 'jquery',
              jQuery: 'jquery',
              'window.jQuery': 'jquery',
              'window.$': 'jquery'
          })
      ]

--- src/todolist.js
+++ src/todolist.js
@@ -1,34 +1,33 @@
-var taskInput = document.getElementById("new-task-input");
-var addButton = document.getElementById("add-task-button");
-var todolistHolder = document.getElementById("todolist-container");
+var taskInput = document.getElementById('new-task-input');
+var addButton = document.getElementById('add-task-button');
+var todolistHolder = document.getElementById('todolist-container');
 
 var createNewTaskElement = function(taskString) {
   //Create List Item
-  var listItem = document.createElement("li");
+  var listItem = document.createElement('li');
 
   //input (checkbox)
-  var checkBox = document.createElement("input"); // checkbox
+  var checkBox = document.createElement('input'); // checkbox
   //label
-  var label = document.createElement("label");
+  var label = document.createElement('label');
   //input (text)
-  var editInput = document.createElement("input"); // text
+  var editInput = document.createElement('input'); // text
   //button.edit
-  var editButton = document.createElement("button");
+  var editButton = document.createElement('button');
   //button.delete
-  var deleteButton = document.createElement("button");
+  var deleteButton = document.createElement('button');
 
   //Each element needs modifying
 
-  checkBox.type = "checkbox";
-  editInput.type = "text";
+  checkBox.type = 'checkbox';
+  editInput.type = 'text';
 
-  editButton.innerText = "Edit";
-  editButton.className = "edit";
-  deleteButton.innerText = "Delete";
-  deleteButton.className = "delete";
+  editButton.innerText = 'Edit';
+  editButton.className = 'edit';
+  deleteButton.innerText = 'Delete';
+  deleteButton.className = 'delete';
 
   label.innerText = taskString;
-
 
   // each element needs appending
   listItem.appendChild(checkBox);
@@ -38,7 +37,7 @@ var createNewTaskElement = function(taskString) {
   listItem.appendChild(deleteButton);
 
   return listItem;
-}
+};
 
 // Add a new task
 var addTask = function() {
@@ -48,26 +47,26 @@ var addTask = function() {
   todolistHolder.insertBefore(listItem, todolistHolder.childNodes[0]);
   bindTaskEvents(listItem, taskCompleted);
   //update taskInput.value
-  taskInput.value = "";
-}
+  taskInput.value = '';
+};
 
 // Edit an existing task
-var editTask = function(){
+var editTask = function() {
   var listItem = this.parentNode;
-  var editInput = listItem.querySelector("input[type=text]");
-  var label = listItem.querySelector("label");
+  var editInput = listItem.querySelector('input[type=text]');
+  var label = listItem.querySelector('label');
 
-  var containsClass = listItem.classList.contains("editMode");
+  var containsClass = listItem.classList.contains('editMode');
 
-  if(containsClass){
+  if (containsClass) {
     label.innerText = editInput.value;
   } else {
     editInput.value = label.innerText;
   }
 
   //Toggle .editMode on the parent
-  listItem.classList.toggle("editMode");
-}
+  listItem.classList.toggle('editMode');
+};
 
 // Delete an existing task
 var deleteTask = function() {
@@ -76,47 +75,47 @@ var deleteTask = function() {
 
   //Remove the parent list item from the ul
   ul.removeChild(listItem);
-}
+};
 
 // Set the click handler to the addTask function
 //addButton.onclick = addTask;
-addButton.addEventListener("click", addTask);
+addButton.addEventListener('click', addTask);
 
 var taskCompleted = function() {
-  console.log("Task complete...");
+  console.log('Task complete...');
   //Append the task list item to the #completed-tasks
   var listItem = this.parentNode;
   //move listItem to the bottom of the to do list
   moveToBottom(listItem);
   bindTaskEvents(listItem, taskIncomplete);
-}
+};
 
 // Mark a task as incomplete
 var taskIncomplete = function() {
-  console.log("Task Incomplete...");
+  console.log('Task Incomplete...');
   // When checkbox is unchecked
   // Append the task list item #incomplete-tasks
   var listItem = this.parentNode;
   //move listItem to the top of the to do list
   moveToTop(listItem);
   bindTaskEvents(listItem, taskCompleted);
-}
+};
 
-var moveToTop = function(taskListItem){
+var moveToTop = function(taskListItem) {
   //insert before list[0]
   todolistHolder.insertBefore(taskListItem, todolistHolder.childNodes[0]);
-}
-var moveToBottom = function(taskListItem){
+};
+var moveToBottom = function(taskListItem) {
   //append to the last item of the to do list
   todolistHolder.appendChild(taskListItem);
-}
+};
 
 var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
-  console.log("Bind list item events");
+  console.log('Bind list item events');
   //select taskListItem's children
-  var checkBox = taskListItem.querySelector("input[type=checkbox]");
-  var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector("button.delete");
+  var checkBox = taskListItem.querySelector('input[type=checkbox]');
+  var editButton = taskListItem.querySelector('button.edit');
+  var deleteButton = taskListItem.querySelector('button.delete');
 
   //bind editTask to edit button
   editButton.onclick = editTask;
@@ -126,17 +125,15 @@ var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
 
   //bind checkBoxEventHandler to checkbox
   checkBox.onchange = checkBoxEventHandler;
-}
-
+};
 
 // Cycle over the incompleteTaskHolder ul list items
-for(var i = 0; i <  todolistHolder.children.length; i++) {
+for (var i = 0; i < todolistHolder.children.length; i++) {
   // bind events to list item's children (taskCompleted)
   bindTaskEvents(todolistHolder.children[i], taskCompleted);
 }
 // Cycle over the completeTaskHolder ul list items
-for(var i = 0; i <  todolistHolder.children.length; i++) {
+for (var i = 0; i < todolistHolder.children.length; i++) {
   // bind events to list item's children (taskIncompleted)
-  bindTaskEvents(todolistHolder.children[i], taskIncomplete); 
-
+  bindTaskEvents(todolistHolder.children[i], taskIncomplete);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+var webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -17,4 +17,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.$': 'jquery',
+    }),
+  ],
 };
