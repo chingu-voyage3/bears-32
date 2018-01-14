bf4cc913afee10e07a96563b7e0427b53c58d05b
Chaim Lando <chiptus@gmail.com>
2018-01-14 17:31:00 +0100

    bundle seperate scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    newtab: './src/newtab/index.js',
+    background: './src/background/index.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'build'),
   },
   module: {
