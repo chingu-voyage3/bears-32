db048fb8f81494621b5e31271a854c9b457f094c
Chaim Lando <chiptus@gmail.com>
2018-03-07 09:26:16 +0200

    load state from local storage and resave

--- src/background/index.js
+++ src/background/index.js
@@ -1,3 +1,21 @@
-import './store';
+import store from './store';
 import { initEventsHandler } from './events-handler';
+
+const LOCAL_STORAGE_STORE_KEY = 'store';
+
 initEventsHandler();
+
+store.subscribe(saveStateInLocalStorage);
+loadStateFromLocalStorage();
+
+function saveStateInLocalStorage(state) {
+  localStorage.setItem(LOCAL_STORAGE_STORE_KEY, JSON.stringify(state));
+}
+
+function loadStateFromLocalStorage() {
+  const stateString = localStorage.getItem(LOCAL_STORAGE_STORE_KEY);
+  if (stateString) {
+    const state = JSON.parse(stateString);
+    store.setState(() => state);
+  }
+}
