295d6cb6e141eb06d7ee4cff9eb1daea9c73bc33
Chaim Lando <chiptus@gmail.com>
2018-03-07 09:40:04 +0200

    move local storage logic into module

--- src/background/index.js
+++ src/background/index.js
@@ -1,21 +1,11 @@
 import store from './store';
 import { initEventsHandler } from './events-handler';
-
-const LOCAL_STORAGE_STORE_KEY = 'store';
+import {
+  loadStateFromLocalStorage,
+  saveStateInLocalStorage,
+} from './local-storage';
 
 initEventsHandler();
 
+loadStateFromLocalStorage(store);
 store.subscribe(saveStateInLocalStorage);
-loadStateFromLocalStorage();
-
-function saveStateInLocalStorage(state) {
-  localStorage.setItem(LOCAL_STORAGE_STORE_KEY, JSON.stringify(state));
-}
-
-function loadStateFromLocalStorage() {
-  const stateString = localStorage.getItem(LOCAL_STORAGE_STORE_KEY);
-  if (stateString) {
-    const state = JSON.parse(stateString);
-    store.setState(() => state);
-  }
-}
