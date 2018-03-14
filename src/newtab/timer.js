359d87c920498cee8f30fbb14405ffd0dd6c40fb
Chaim Lando <chiptus@gmail.com>
2018-03-14 16:06:22 +0200

    use state for running timer

--- src/newtab/timer.js
+++ src/newtab/timer.js
@@ -1,22 +1,6 @@
 import store from './store';
 import { startTimer, stopTimer } from './actions';
 
-let currentSession = null;
-
-store.subscribe(state => {
-  const { currentSessionId } = state.timer;
-  // don't do anything if the session didn't change
-  if (currentSession && currentSession.id === currentSessionId) {
-    return;
-  }
-  // check if currentSessionId is valid
-  if (!state.sessions.ids.includes(currentSessionId)) {
-    return;
-  }
-  currentSession = state.sessions.byId[currentSessionId];
-  runTimer();
-});
-
 // Connect to DOM
 const playBtn = document.querySelector('.playBtn');
 // const pauseBtn = document.querySelector('.pauseBtn');
@@ -24,13 +8,32 @@ const resetBtn = document.querySelector('.resetBtn');
 const minutesSpan = document.querySelector('.minutes');
 const secondsSpan = document.querySelector('.seconds');
 
+let currentSession = null;
+let timerRunningTimeout = null;
+
+store.subscribe(state => {
+  const { currentSessionId } = state.timer;
+
+  if (!currentSessionId) {
+    // hide the reset button
+  }
+  // check if currentSessionId is valid
+  if (!state.sessions.ids.includes(currentSessionId)) {
+    return;
+  }
+
+  currentSession = state.sessions.byId[currentSessionId];
+});
+
 // Set event listeners on buttons
 playBtn.addEventListener('click', () => startTimer());
 // pauseBtn.addEventListener('click', pauseTimer);
-resetBtn.addEventListener('click', () => stopTimer());
+resetBtn.addEventListener('click', () => stopTimer(currentSession.id));
 
 // Initialize page onload
-export function initTimer() {}
+export function initTimer() {
+  runTimer();
+}
 
 // Timer Helpers ------------------------------------------
 
@@ -39,8 +42,8 @@ function getTimeRemaining(endtime) {
   // milliseconds
   const total = endtime - Date.now();
 
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = total > 0 ? Math.floor((total / 1000 / 60) % 60) : 0;
+  const seconds = total > 0 ? Math.floor((total / 1000) % 60) : 0;
 
   return {
     total,
@@ -52,18 +55,21 @@ function getTimeRemaining(endtime) {
 // Timer Controls ------------------------------------------
 
 function runTimer() {
-  let timerRunning = setInterval(timerRefresh, 1000);
+  timerRunningTimeout = setInterval(timerRefresh, 1000);
   timerRefresh();
 
   // Refresh timer every 100th second with current countdown
   function timerRefresh() {
-    let { minutes, total, seconds } = getTimeRemaining(currentSession.end);
-    minutesSpan.innerHTML = ('0' + minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + seconds).slice(-2);
-
-    if (total <= 0) {
-      clearInterval(timerRunning);
-      timerRunning = null;
+    if (!currentSession) {
+      return;
     }
+    let { minutes, seconds, total } = getTimeRemaining(currentSession.end);
+    // console.log(currentSession, minutes, total, seconds);
+    if (total < 0) {
+      // ask for what happend in this session...
+    }
+
+    minutesSpan.innerHTML = String(minutes).padStart(2, 0);
+    secondsSpan.innerHTML = String(seconds).padStart(2, 0);
   }
 }
